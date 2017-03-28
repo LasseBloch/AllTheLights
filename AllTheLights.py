2c58657180a3fbe5b564d6739050b260f4278e9d
LasseBloch <lassebloch@gmail.com>
2017-03-28 19:27:21 +0200

    Added hart beat and check if light already exist

--- AllTheLights.py
+++ AllTheLights.py
@@ -1,31 +1,47 @@
+import time
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 lights = []
 
+
+def name_already_excists(name):
+    for light in lights:
+        if light['name'] == name:
+            return True
+    return False
+
 @app.route('/lights/list', methods=['GET'])
 def get_lights():
     return jsonify({'lights': lights})
 
 @app.route('/lights/add', methods=['POST'])
+# Add also works as keep alive if we already have the light in the dictionary
 def add_light():
     if not request.json or not 'name' in request.json:
         return '', 404
-
+    #    if lights
     if len(lights):
-        id = lights[-1]['id'] + 1
+        if not name_already_excists(request.json['name']):
+            id = lights[-1]['id'] + 1
+        else:
+            print "{} already in list of lights".format(request.json['name'])
+            # 409 Conflict if the name is already taken
+            return '', 409
     else:
         id = 0
 
     new_light = {
         'id': id,
         'name': request.json['name'],
-        'value': 0
+        'value': 0,
+        'last_hart_beat': time.time()
     }
     lights.append(new_light)
     request.json
-    return ''
+    return '', 201
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
